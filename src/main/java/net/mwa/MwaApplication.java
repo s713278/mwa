1de058a794b00c94b64d364105eb220cc1bee1ad
SwamyK <swamy.kunta@gmail.com>
2017-12-23 08:24:24 +0530

    Turned off redis session

--- src/main/java/net/mwa/MwaApplication.java
+++ src/main/java/net/mwa/MwaApplication.java
@@ -3,11 +3,10 @@ package net.mwa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class MwaApplication {
 
 	public static void main(String[] args) {
